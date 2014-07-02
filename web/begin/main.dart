8f77b5f51fea5503e5d3288420d7db8836a2c7aa
Aaron Madison <aaron.madison@imtapps.com>
2014-07-02 06:36:00 -0500

    cleaned up steps 1-4

--- web/begin/main.dart
+++ web/begin/main.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 
 void main() {
  Element element = querySelector(".content");
+ 
  for (var i = 0; i < 200; i++) {
    element.innerHtml += "Hello, world!<br />";
  }
--- web/step4/codelab-app.dart
+++ web/step4/codelab-app.dart
@@ -1,8 +1,5 @@
 import 'package:polymer/polymer.dart';
-import 'package:core_elements/core_drawer_panel.dart' show CoreDrawerPanel;
 import 'dart:html';
-import 'dart:js' show JsObject, context;
-
 
 
 @CustomTag('codelab-app')
