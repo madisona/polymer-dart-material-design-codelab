b5c058551447d285d6ae70af045981d3ea2a94b1
Aaron Madison <aaron.madison@imtapps.com>
2014-11-09 05:05:04 -0600

    Feature: Completed Step 7 - adding side menu options

---
+++ web/step7/codelab-app.dart
@@ -0,0 +1,69 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:convert' show JSON;
+
+import 'model.dart' show Note;
+
+
+const storageKey = "storage";
+
+
+@CustomTag('codelab-app')
+class CodelabApp extends PolymerElement {
+  @observable List data = toObservable([]);
+
+  @observable String newNote;
+  @observable bool fadeSelected = true;
+  @observable var fontSize = '14';
+  Storage localStorage = window.localStorage;
+
+  CodelabApp.created() : super.created() {
+    // adding a new note should be hidden by default.
+    $['newNoteInput'].style.display = 'none';
+
+    // populate data initially from the local storage.
+    if (localStorage.containsKey(storageKey)) {
+        for (var item in JSON.decode(localStorage[storageKey])) {
+          data.add(new Note.fromJson(item));
+        }
+    }
+  }
+
+  void showNewNoteInput() {
+    $['newNoteInput'].style.display = 'block';
+  }
+
+  void toggleDrawer() {
+    $['drawerPanel'].togglePanel();
+  }
+
+  void dataChanged(val) {
+    localStorage[storageKey] = JSON.encode(data);
+  }
+
+  void fontSizeChanged() {
+      var cards = this.shadowRoot.querySelectorAll('.card');
+      for (var card in cards) {
+        card.style.fontSize = fontSize.toString() + "px";
+      }
+  }
+
+  void add(Event e, Object detail, Node sender) {
+    e.preventDefault();;
+    if (sender.value != null) {
+      data.insert(0, new Note(sender.value, false));
+      $['newNoteInput'].style.display = 'none';
+      $['newNoteInput'].value = null;
+    }
+  }
+
+  void delete() {
+    data.removeWhere((note) => note.done == true);
+  }
+
+  void reset() {
+    fadeSelected = true;
+    fontSize = "14";
+    $['toast'].show();
+  }
+}
